51cb0e2e79f3ab7982def0eca8b53cfe88a3fc2c
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2026-04-09 14:40:54 +0545

    add JWT token generation function to security utilities

--- backend/core/security.py
+++ backend/core/security.py
@@ -2,6 +2,10 @@
 # such as password hashing and verification.
 
 from passlib.context import CryptContext
+from datetime import datetime, timedelta, timezone
+from jose import JWTError, jwt
+from core.config import settings
+from typing import Optional
 
 # Create a CryptContext for password hashing using the bcrypt algorithm
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -15,4 +19,28 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
     # Verifies a plaintext password against a hashed password.
     plain_password = plain_password[:72]
     return pwd_context.verify(plain_password, hashed_password)
+
+
+#JWT token generation function
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    # Creates a JWT access token with the provided data and expiration time.
+    to_encode = data.copy()
+    
+    # Set the expiration time for the token. If expires_delta is provided, use it; otherwise, use the default expiration time from settings.
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes = settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    
+    # Add the expiration time to the token payload
+    to_encode.update({"exp": expire})
+    
+    # Encode the token using the secret key and algorithm specified in settings
+    encoded_jwt = jwt.encode(
+        to_encode, 
+        settings.SECRET_KEY, 
+        algorithm=settings.ALGORITHM
+    )
+    
+    return encoded_jwt
 
