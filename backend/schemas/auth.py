542159eb1a9851a0e600b2f8aad0bd5e7b2b7851
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2026-04-11 15:19:13 +0545

    refactor(auth): enhance UserCreate model with additional optional fields for dob, gender, and phone

--- backend/schemas/auth.py
+++ backend/schemas/auth.py
@@ -2,13 +2,19 @@
 # These models are used for validating and serializing data in API requests and responses.
 # These are input validation models for user creation and authentication processes.
 
-
+from typing import Optional
 from pydantic import BaseModel, EmailStr
 
 class UserCreate(BaseModel):
-    name: str  # User's full name
-    email: EmailStr  # User's email address, validated as an email format
-    password: str  # User's plaintext password
-    role: str  # User's role (e.g., "user", "admin")
+    name: str
+    email: EmailStr
+    password: str
+    role: str  # Role can be "patient" or "doctor"
+    dob: Optional[str] # Date of Birth (optional, can be added later)
+    gender: Optional[str] # Gender (optional, can be added later)
+    phone: Optional[str] # Phone number (optional, can be added later)
     
     
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str   
