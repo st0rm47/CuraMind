cfa02752914d180e1b80b46f1ee74af52e703003
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2026-04-21 12:51:59 +0545

    feat(config): add DEBUG and SYNC_DATABASE_URL settings to configuration

--- backend/core/config.py
+++ backend/core/config.py
@@ -5,7 +5,10 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     APP_NAME: str = "CuraMind"  # Name of the application
     APP_VERSION: str = "1.0.0"  # Version of the application
+    DEBUG: bool = False  # Debug mode flag, set to True for development and False for production
+    
     DATABASE_URL: str  # Database connection URL, loaded from environment variable
+    SYNC_DATABASE_URL: str  # Synchronous database URL for Alembic migrations, loaded from environment variable
     
     SECRET_KEY: str  # Secret key for security-related operations, loaded from environment variable
     ALGORITHM: str = "HS256"  # Algorithm used for token encoding (e.g., JWT)
@@ -13,6 +16,7 @@ class Settings(BaseSettings):
     
     class Config:
         env_file = ".env"  # Load environment variables from the .env file
+        case_sensitive = True  # Environment variable names are case-sensitive
         
 # Create an instance of the Settings class to access the configuration values
 settings = Settings()
